8de933f86707e294779052819bfa8ec39e6b7e2b
codedependant74 <brendale74@gmail.com>
2022-09-22 22:30:21 -0400

    Working Game

---
+++ README.md
@@ -0,0 +1,24 @@
+# Kei's Classic: Pong Game
+
+## Description
+
+This is a classic Pong Game made to resemble a tennis court. It is a multiplayer game with player 1 using the 'w' & 's' keys as up and down to move the left paddle, respectfully. Player 2 uses the up and down arrow keys to move the right paddle.
+
+### To play game:
+
+1. press the space bar to start the game
+2. use respective player keys to move paddle up and down to hit the ball.
+3. Each time the ball is hit, the speed increases. When a player misses the speed resets.
+4. First player to 5 wins the game.
+5. To exit or restart game, press the restart button, then click on the game and press space bar.
+
+## Tech Stack
+
+    - HTML
+    - CSS
+    - JavaScript
+
+## Upcoming Features
+
+    - Option to play multiplayer or versus computer
+    - Game over screen to display winner
--- script.js
+++ script.js
@@ -1,170 +1,222 @@
-const canvas = document.getElementById("pongGame");
-const context = canvas.getContext("2d");
-canvas.width = 850;
-canvas.height = 600;
+const gameBoard = document.querySelector("#pongGame");
+gameBoard.width = 850;
+gameBoard.height = 500;
+const ctx = gameBoard.getContext("2d");
+const scoreText = document.querySelector("#scoreText");
+const resetBtn = document.querySelector("#resetBtn");
+const popup = document.getElementById("popup");
+const startBtn = document.querySelector("#startBtn");
+const gameWidth = gameBoard.width;
+const gameHeight = gameBoard.height;
+const boardBackground = "#1E8FD5";
+const paddle1Color = "#fff";
+const paddle2Color = "#a0d6b4";
+const paddleBorder = "black";
+const ballColor = "#ccff00";
+const ballBorderColor = "#fff";
+const ballRadius = 12.5;
+const paddleSpeed = 50;
+let intervalID;
+let ballSpeed;
+let ballX = gameWidth / 2;
+let ballY = gameHeight / 2;
+let ballXDirection = 0;
+let ballYDirection = 0;
+let player1Score = 0;
+let player2Score = 0;
+let paddle1 = {
+  width: 25,
+  height: 100,
+  x: 0,
+  y: 0,
+};
+let paddle2 = {
+  width: 25,
+  height: 100,
+  x: gameWidth - 25,
+  y: gameHeight - 100,
+};
 
-let scoreOne = 0;
-let scoreTwo = 0;
+window.addEventListener("keydown", changeDirection);
+resetBtn.addEventListener("click", resetGame);
 
-// key movement
-window.addEventListener("keydown", doKeyDown);
-function doKeyDown(e) {
-  const key = e.key;
-  if (key == "SpaceBar") {
-  }
-  if (key == "w" && playerOne.y - playerOne.gravity > 0) {
-    playerOne.y -= playerOne.gravity * 4;
-  } else if (
-    key == "s" &&
-    playerOne.y + playerOne.height + playerOne.gravity < canvas.height
-  ) {
-    playerOne.y += playerOne.gravity * 4;
-  }
-  if (key == "ArrowUp" && playerTwo.y - playerTwo.gravity > 0) {
-    playerTwo.y -= playerTwo.gravity * 4;
-  } else if (
-    key == "ArrowDown" &&
-    playerTwo.y + playerTwo.height + playerTwo.gravity < canvas.height
-  ) {
-    playerTwo.y += playerTwo.gravity * 4;
-  }
+function gameStart() {
+  createBall();
+  nextTick();
 }
-
-class Element {
-  constructor(options) {
-    this.x = options.x;
-    this.y = options.y;
-    this.width = options.width;
-    this.height = options.height;
-    this.color = options.color;
-    this.speed = options.speed || 2;
-    this.gravity = options.gravity;
-  }
+function nextTick() {
+  intervalID = setTimeout(() => {
+    clearBoard();
+    drawPaddles();
+    moveBall();
+    drawBall(ballX, ballY);
+    checkCollision();
+    nextTick();
+  }, 10);
 }
-
-// first paddle
-const playerOne = new Element({
-  // use constructor to name element and give its properties
-  x: 10,
-  y: 200,
-  width: 15,
-  height: 80,
-  color: "#fff",
-  gravity: 2,
-});
-
-//second paddle
-const playerTwo = new Element({
-  // use constructor to name element and give its properties
-  x: 825,
-  y: 200,
-  width: 15,
-  height: 80,
-  color: "#a0d6b4",
-  gravity: 2,
-});
-
-//ball
-const ball = new Element({
-  // use constructor to name element and give its properties
-  // creates the ball's look
-  x: 850 / 2,
-  y: 600 / 2,
-  width: 15,
-  height: 15,
-  color: "#ccff00",
-  speed: 1,
-  gravity: 1,
-});
-
-// draw elements
-function drawElement(element) {
-  // this is code draws the element visually
-  context.fillStyle = element.color;
-  context.fillRect(element.x, element.y, element.width, element.height);
+function clearBoard() {
+  ctx.fillStyle = boardBackground;
+  ctx.fillRect(0, 0, gameWidth, gameHeight);
 }
+function drawPaddles() {
+  ctx.strokeStyle = paddleBorder;
 
-// Player one score text
-function displayScoreOne() {
-  context.font = "40px Arial";
-  context.fillStyle = "#fff";
-  context.fillText(scoreOne, canvas.width / 2 - 60, 30);
-}
-// Player 2 score text
-function displayScoreTwo() {
-  context.font = "40px Arial";
-  context.fillStyle = "#fff";
-  context.fillText(scoreTwo, canvas.width / 2 + 60, 30);
-}
+  ctx.fillStyle = paddle1Color;
+  ctx.fillRect(paddle1.x, paddle1.y, paddle1.width, paddle1.height);
+  ctx.strokeRect(paddle1.x, paddle1.y, paddle1.width, paddle1.height);
 
-// detect collision
-function ballWallCollision() {
-  if (
-    (ball.y + ball.gravity <= playerTwo.y + playerTwo.height &&
-      ball.x + ball.width + ball.speed >= playerTwo.x &&
-      ball.y + ball.gravity > playerTwo.y) ||
-    (ball.y + ball.gravity > playerOne.y &&
-      ball.x + ball.speed <= playerOne.x + playerOne.width)
-  ) {
-    ball.speed = ball.speed * -1;
-  } else if (ball.x + ball.speed < playerOne.x) {
-    scoreTwo += 1;
-    ball.speed = ball.speed * -1;
-    ball.x = 100 + ball.speed;
-    ball.y += ball.gravity;
-  } else if (ball.x + ball.speed > playerTwo.x + playerTwo.width) {
-    scoreOne += 1;
-    ball.speed = ball.speed * -1;
-    ball.x = 100 + ball.speed;
-    ball.y += ball.gravity;
-  } // LEFT OFF HERE, HAVE TO GET THE BALL TO MOVE
-  return drawElements();
+  ctx.fillStyle = paddle2Color;
+  ctx.fillRect(paddle2.x, paddle2.y, paddle2.width, paddle2.height);
+  ctx.strokeRect(paddle2.x, paddle2.y, paddle2.width, paddle2.height);
 }
-// make ball bounce
-function ballBounce() {
-  if (ball.y + ball.gravity <= 0 || ball.y + ball.gravity >= canvas.height) {
-    ball.gravity = ball.gravity * -1;
-    ball.y += ball.gravity;
-    ball.x += ball.speed;
+function createBall() {
+  ballSpeed = 1;
+  if (Math.round(Math.random()) == 1) {
+    ballXDirection = 1;
   } else {
-    ball.y += ball.gravity;
-    ball.x += ball.speed;
+    ballXDirection = -1;
   }
-  return ballWallCollision();
+  if (Math.round(Math.random()) == 1) {
+    ballYDirection = Math.random() * 1; //more random directions
+  } else {
+    ballYDirection = Math.random() * -1; //more random directions
+  }
+  ballX = gameWidth / 2;
+  ballY = gameHeight / 2;
+  drawBall(ballX, ballY);
 }
-// draw all elements
-function drawElements() {
-  //use lineTo to draw lines on canvas to mimic tennis court
-  context.beginPath();
-  context.moveTo(101, 200);
-  context.lineTo(290, 199);
-  context.moveTo(503, 202);
-  context.lineTo(290, 200);
-  context.moveTo(500, 43);
-  context.lineTo(501, 361);
-  context.moveTo(87, 44);
-  context.lineTo(87, 359);
-  context.moveTo(1, 45);
-  context.lineTo(601, 45);
-  context.moveTo(1, 357);
-  context.lineTo(600, 357);
-  context.moveTo(599, 45);
-  context.lineTo(600, 358);
-  context.moveTo(-1, 47);
-  context.lineTo(-1, 358);
-  context.moveTo(297, 1);
-  context.lineTo(296, 399);
-  context.strokeStyle = "white";
-  context.stroke();
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  drawElement(playerOne);
-  drawElement(playerTwo);
-  drawElement(ball);
-  displayScoreOne();
-  displayScoreTwo();
+function moveBall() {
+  ballX += ballSpeed * ballXDirection;
+  ballY += ballSpeed * ballYDirection;
 }
-function loop() {
-  ballBounce();
-  window.requestAnimationFrame(loop);
+function drawBall(ballX, ballY) {
+  ctx.fillStyle = ballColor;
+  ctx.strokeStyle = ballBorderColor;
+  ctx.lineWidth = 2;
+  ctx.beginPath();
+  ctx.arc(ballX, ballY, ballRadius, 0, 2 * Math.PI);
+  ctx.stroke();
+  ctx.fill();
 }
-loop();
+function checkCollision() {
+  if (ballY <= 0 + ballRadius) {
+    ballYDirection *= -1;
+  }
+  if (ballY >= gameHeight - ballRadius) {
+    ballYDirection *= -1;
+  }
+  if (ballX <= 0) {
+    player2Score += 1;
+    updateScore();
+    createBall();
+    return;
+  }
+  if (ballX >= gameWidth) {
+    player1Score += 1;
+    updateScore();
+    createBall();
+    return;
+  }
+  if (ballX <= paddle1.x + paddle1.width + ballRadius) {
+    if (ballY > paddle1.y && ballY < paddle1.y + paddle1.height) {
+      ballX = paddle1.x + paddle1.width + ballRadius; // if ball gets stuck
+      ballXDirection *= -1;
+      ballSpeed += 1;
+    }
+  }
+  if (ballX >= paddle2.x - ballRadius) {
+    if (ballY > paddle2.y && ballY < paddle2.y + paddle2.height) {
+      ballX = paddle2.x - ballRadius; // if ball gets stuck
+      ballXDirection *= -1;
+      ballSpeed += 1;
+    }
+  }
+}
+function changeDirection(event) {
+  const keyPressed = event.keyCode;
+  const start = 32;
+  const paddle1Up = 87;
+  const paddle1Down = 83;
+  const paddle2Up = 38;
+  const paddle2Down = 40;
+
+  switch (keyPressed) {
+    case start:
+      if (start == keyPressed) {
+        gameStart();
+      }
+    case paddle1Up:
+      if (paddle1.y > 0) {
+        paddle1.y -= paddleSpeed;
+      }
+      break;
+    case paddle1Down:
+      if (paddle1.y < gameHeight - paddle1.height) {
+        paddle1.y += paddleSpeed;
+      }
+      break;
+    case paddle2Up:
+      if (paddle2.y > 0) {
+        paddle2.y -= paddleSpeed;
+      }
+      break;
+    case paddle2Down:
+      if (paddle2.y < gameHeight - paddle2.height) {
+        paddle2.y += paddleSpeed;
+      }
+      break;
+  }
+}
+// function stopGame() {
+//   clearInterval(gamestart);
+// }
+function updateScore() {
+  scoreText.textContent = `${player1Score} : ${player2Score}`;
+  if (player1Score === 5 || player2Score === 5) {
+    resetGame();
+  }
+}
+function resetGame() {
+  player1Score = 0;
+  player2Score = 0;
+  paddle1 = {
+    width: 25,
+    height: 100,
+    x: 0,
+    y: 0,
+  };
+  paddle2 = {
+    width: 25,
+    height: 100,
+    x: gameWidth - 25,
+    y: gameHeight - 100,
+  };
+  ballSpeed = 1;
+  ballX = 0;
+  ballY = 0;
+  ballXDirection = 0;
+  ballYDirection = 0;
+  updateScore();
+  clearInterval(intervalID);
+  clearBoard();
+}
+
+//   context.beginPath();
+//   context.moveTo(101, 200);
+//   context.lineTo(290, 199);
+//   context.moveTo(503, 202);
+//   context.lineTo(290, 200);
+//   context.moveTo(500, 43);
+//   context.lineTo(501, 361);
+//   context.moveTo(87, 44);
+//   context.lineTo(87, 359);
+//   context.moveTo(1, 45);
+//   context.lineTo(601, 45);
+//   context.moveTo(1, 357);
+//   context.lineTo(600, 357);
+//   context.moveTo(599, 45);
+//   context.lineTo(600, 358);
+//   context.moveTo(-1, 47);
+//   context.lineTo(-1, 358);
+//   context.moveTo(297, 1);
+//   context.lineTo(296, 399);
